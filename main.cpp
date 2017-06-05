d6862aa521e3b3e2aec7c913707df49774225b56
Terran <messi93@126.com>
2017-06-05 13:44:53 -0700

    some manual codes for a particular Marc file

--- main.cpp
+++ main.cpp
@@ -16,8 +16,26 @@ int main()
     mfr.ReadMarc(sam);
     mfr.ReadMatcode(sam);
 
+    //manually assign values
     sam->units->force="N";
     sam->units->length="mm";
+
+    sam->property->sections.rbegin()->Jx=2.5e9;
+    sam->property->sections.rbegin()->G=11667;
+    Fiber fiber;
+    fiber.material="elastic";
+    fiber.area=120000;
+    fiber.zCoord=0;
+    fiber.zCoord=0;
+    sam->property->sections.rbegin()->fibers.push_back(fiber);
+    Material mat;
+    mat.nu=0.2;
+    mat.rho=1.0;
+    mat.name="elastic";
+    mat.type="elastic";
+    mat.Ec=28000;
+    sam->property->materials.push_back(mat);
+
 
     sam->WriteJson("output.json");
 
